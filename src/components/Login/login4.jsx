918fbb940f6f9b73885cc2409b0038074b6e5a65
athulsreekumar <athult2000@gmail.com>
2024-01-23 18:08:06 +0530

    added credentials

--- src/components/Login/login4.jsx
+++ src/components/Login/login4.jsx
@@ -42,6 +42,7 @@ export default function Login({ onLogin, loggedInUser, setLoggedInUser }) {
             headers: {
                 'Content-Type': 'application/json',
             },
+            credentials: 'include',
             body: JSON.stringify(dataToSubmit),
         })
             // .then((res) => res.json())
