05d22aa6f8be3e92750bb1c9bd786c62c28ead00
athulsreekumar <athult2000@gmail.com>
2024-01-23 18:37:54 +0530

    removed baseURL

--- src/components/Login/login4.jsx
+++ src/components/Login/login4.jsx
@@ -37,7 +37,7 @@ export default function Login({ onLogin, loggedInUser, setLoggedInUser }) {
 
         console.log(dataToSubmit)
 
-        fetch('https://anyonyam.onrender.com/login', {
+        fetch('/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
